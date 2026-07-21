e688e9c56d45dafddcad6680e76ff6da0adbb733
Marilyn Pious <marilynpious@gmail.com>
2026-07-21 09:55:44 +0530

    Completed week 5 hands on

---
+++ week5/trainerapp_6/src/TrainersMock.js
@@ -0,0 +1,30 @@
+import Trainer from "./Trainer";
+
+const trainers = [
+    new Trainer(
+        1,
+        "John",
+        "john@gmail.com",
+        "9876543210",
+        "React",
+        "React, JS"
+    ),
+    new Trainer(
+        2,
+        "Alice",
+        "alice@gmail.com",
+        "9876501234",
+        "Java",
+        "Spring Boot"
+    ),
+    new Trainer(
+        3,
+        "David",
+        "david@gmail.com",
+        "9876512345",
+        "Angular",
+        "Angular, TS"
+    )
+];
+
+export default trainers;
---
+++ week5/trainerapp_6/src/components/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <h2>Welcome to Trainers App</h2>
+        </div>
+    );
+}
+
+export default Home;
